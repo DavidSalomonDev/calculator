eb1d40a9c4249470ef67979671b8c7de46405b90
salo <salo@david-salomon.com>
2021-04-15 15:35:54 -0600

    🎨 feat: desing elements (buttons and screen) for calculator

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+    Button,
+    Container,
+    Current,
+    Previous,
+    Screen
+} from './Styled'
+
+function Calculator() {
+    return ( 
+        <Container >
+            <Screen>
+                <Previous>10</Previous>
+                <Current>3232</Current>
+            </Screen>
+            <Button gridSpan={2} control> AC </Button>
+            <Button control>DEL</Button>
+            <Button operation>÷</Button>
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button operation>x</Button>
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button operation>+</Button>
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button operation>-</Button>
+            <Button period>.</Button>
+            <Button >0</Button>
+            <Button gridSpan={2} equals> = </Button>
+        </Container>
+    )
+}
+
+export default Calculator
